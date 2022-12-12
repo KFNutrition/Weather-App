522d9c5f679947633abeac679168ad9d03412e7b
KFNutrition <119586694+KFNutrition@users.noreply.github.com>
2022-12-12 22:19:08 +1100

    Updated API & Times

--- src/app.js
+++ src/app.js
@@ -1,1 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    minutes = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
+function displayTemperature(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#windspeed");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+}
+
+let apikey = "bbaea6o6503ffbf53tac053a514b1a25";
+let query = "Lisbon";
+let apiurl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apikey}&units=metric`;
+axios.get(apiurl).then(displayTemperature);
